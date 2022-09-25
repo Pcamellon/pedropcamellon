6ce3bd72d09e4eaf929a030b130608fb93be1b43
Pcamellon <pedropcamellon@gmail.com>
2022-09-25 10:28:40 -0400

    initial commit

--- pages/index.js
+++ pages/index.js
@@ -37,6 +37,10 @@ export default function Index({ preview, allPosts }) {
 export async function getStaticProps({ preview = false }) {
   const allPosts = (await getAllPostsForHome(preview)) ?? [];
   return {
-    props: { preview, allPosts }
+    props: {
+      preview,
+      allPosts
+    },
+    revalidate: 5
   };
 }
